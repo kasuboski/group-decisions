ad13f7c597840711081e4d19a196eef3cbdf16a1
Josh Kasuboski <kasuboski@wisc.edu>
2017-10-14 17:19:36 -0500

    Add yourself to list

--- src/components/List.js
+++ src/components/List.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const style = {
   listStyle: 'none',
+  padding: 0,
 };
 
 const List = props => (
--- src/infoReducer.js
+++ src/infoReducer.js
@@ -1,14 +1,17 @@
 const initialState = {
-    room: '',
-    name: '',
-    otherPlayers: [], // {name, status}
+  room: '',
+  name: '',
+  otherPlayers: [], // {name, status}
 }
 
 export default function reducer(state = initialState, action) {
-    switch(action.type) {
-        case 'JOIN_ROOM':
-            return {...state, ...{ room: action.room, name: action.name }};
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case 'JOIN_ROOM':
+      const newPlayers = [...state.otherPlayers, {name: action.name}];
+      console.log(newPlayers);
+      const newState = { room: action.room, name: action.name, otherPlayers: newPlayers };
+      return { ...state, ...newState };
+    default:
+      return state;
+  }
 }
