2bdfbff6881d4cd1dde0db9d21c78136ef79373d
Josh Kasuboski <josh.kasuboski@gmail.com>
2018-01-15 15:05:03 -0600

    limit form logging

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -5,7 +5,10 @@ import rootReducer from './rootReducer';
 const middlewares = [thunkMiddleware];
 
 if (process.env.NODE_ENV === `development`) {
-    const { logger } = require(`redux-logger`);
+    const { createLogger } = require(`redux-logger`);
+    const logger = createLogger({
+      predicate: (getState, action) => !action.type.startsWith('@@redux-form'),
+    });
   
     middlewares.push(logger);
   }
