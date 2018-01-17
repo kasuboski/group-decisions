a92e31a8832a489f6e5d41afffbfd7b879987ecb
Josh Kasuboski <josh.kasuboski@gmail.com>
2018-01-16 18:55:02 -0600

    Fix spacing

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -6,12 +6,12 @@ import rootReducer from './rootReducer';
 const middlewares = [thunkMiddleware];
 
 if (process.env.NODE_ENV === `development`) {
-    const { createLogger } = require(`redux-logger`);
-    const logger = createLogger({
-      predicate: (getState, action) => !action.type.startsWith('@@redux-form'),
-    });
-  
-    middlewares.push(logger);
-  }
+  const { createLogger } = require(`redux-logger`);
+  const logger = createLogger({
+    predicate: (getState, action) => !action.type.startsWith('@@redux-form'),
+  });
+
+  middlewares.push(logger);
+}
 
 export default createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
